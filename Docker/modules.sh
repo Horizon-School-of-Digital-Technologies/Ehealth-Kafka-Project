2777b65db82cf4963087f4f8c9a4e8736c3801fc
nadine_laabidi <100295828+nadinelabidi@users.noreply.github.com>
2022-07-02 20:15:37 +0100

    Add files via upload

---
+++ Docker/modules.sh
@@ -0,0 +1,10 @@
+#import necessary modules to run classification ML model : XGBoost
+from platform import python_branch
+from tarfile import PAX_NAME_FIELDS
+
+
+pip install scikit-learn==1.0.2
+pip install xgboost
+pip install category_encoders
+pip install kafka-python
+pip install pandas        
