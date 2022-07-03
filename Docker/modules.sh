9f256a9c5878dc253a37c4829f903f08eaf26827
nadine_laabidi <100295828+nadinelabidi@users.noreply.github.com>
2022-07-03 16:14:36 +0100

    Update modules.sh

--- Docker/modules.sh
+++ Docker/modules.sh
@@ -1,8 +1,8 @@
 #import necessary modules to run classification ML model : XGBoost
-sudo apt install python3-pip python3-dev
-pip install scikit-learn==1.0.2
-pip install xgboost
-pip install category_encoders
-pip install kafka-python
-pip install pandas  
+sudo apt install python3-pip python3-dev "\n"
+pip install scikit-learn==1.0.2 "\n"
+pip install xgboost "\n"
+pip install category_encoders "\n"
+pip install kafka-python "\n"
+pip install pandas  "\n"
 
