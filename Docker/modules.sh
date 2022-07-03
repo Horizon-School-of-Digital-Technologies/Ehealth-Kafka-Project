021d3bd4c234f71d17342ecba82496b10fb81dd1
nadine_laabidi <100295828+nadinelabidi@users.noreply.github.com>
2022-07-03 16:10:48 +0100

    Update modules.sh

--- Docker/modules.sh
+++ Docker/modules.sh
@@ -1,7 +1,4 @@
 #import necessary modules to run classification ML model : XGBoost
-from platform import python_branch
-from tarfile import PAX_NAME_FIELDS
-
 
 pip install scikit-learn==1.0.2
 pip install xgboost
